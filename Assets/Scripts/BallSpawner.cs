38983e61364c8040f5e69ef58cfe0bc9dfff4ae2
Jonathan Obeid <jonathan_18_2000@hotmail.com>
2019-02-27 11:07:45 -0500

    Tarerm

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -9,7 +9,7 @@ public class BallSpawner : NetworkBehaviour
     public bool EstCrée = false;
     public override void OnStartServer()
     {
-        GameObject balleJeu = Instantiate(Balle, new Vector3(0, 1, 0), Quaternion.identity);
+        GameObject balleJeu = (GameObject)Instantiate(Balle, new Vector3(0, 1, 0), Quaternion.identity);
         balleJeu.name = "Balle";
         NetworkServer.Spawn(balleJeu);
         EstCrée = true;
