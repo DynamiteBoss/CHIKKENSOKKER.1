69137a6fae5bccd9c1df8f8075d90fd4f193921c
Avex89 <47183943+Avex89@users.noreply.github.com>
2019-03-10 13:23:57 -0400

    BalleSpawnerV2

---
+++ Assets/Scripts/BallSpawnerV2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BallSpawnerV2 : NetworkBehaviour
+{
+    public GameObject ballePrefab;
+
+    public override void OnStartServer()
+    {
+        Vector3 spawnPos = new Vector3(0, 0.5f, 0);
+        Quaternion spawnRot = Quaternion.Euler(0, 0, 0);
+
+        GameObject balle = (GameObject)Instantiate(ballePrefab, spawnPos, spawnRot);
+        NetworkServer.Spawn(balle);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
