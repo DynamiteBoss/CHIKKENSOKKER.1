9ca766e342bd8656d10f610b551cf889d4098cb8
Avex89 <47183943+Avex89@users.noreply.github.com>
2019-04-01 14:37:25 -0400

    commencement changergardien

---
+++ Assets/Scripts/ChangerContrôleGardien.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangerContrôleGardien : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject b = GameObject.Find("Joueur");
+        b.transform.tag = "gardien";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
