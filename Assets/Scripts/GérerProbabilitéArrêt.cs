f23e0c904887d28bd8851a7ab014a3f33b686e70
Avex89 <47183943+Avex89@users.noreply.github.com>
2019-04-20 15:03:51 -0400

    reset ordi

---
+++ Assets/Scripts/GérerProbabilitéArrêt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GérerProbabilitéArrêt : MonoBehaviour
+{
+    GameObject Balle { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Balle = GameObject.FindGameObjectWithTag("Balle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+  
+}
