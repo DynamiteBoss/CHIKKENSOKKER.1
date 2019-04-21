cd6a34111eaf27036b892f251732179863efc382
Sterling-qc <yologuy974@gmail.com>
2019-04-21 16:58:59 -0400

    Update ScriptMécaniqueMatch.cs

--- Assets/Scripts/ScriptMécaniqueMatch.cs
+++ Assets/Scripts/ScriptMécaniqueMatch.cs
@@ -236,7 +236,7 @@ public class ScriptMécaniqueMatch : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (/*GameObject.FindGameObjectsWithTag("AI").Length > 3*/true)
+        if (/*GameObject.FindGameObjectsWithTag("AI").Length > 3*/true)   //TEMPORAIRE
         {
             Debug.Log("Allo");
             if (matchEnCours)
