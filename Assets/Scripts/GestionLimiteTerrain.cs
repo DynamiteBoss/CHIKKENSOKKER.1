087ce0390011849d21a2a45009bc88d7d7aea196
Jonathan Obeid <jonathan_18_2000@hotmail.com>
2019-04-24 12:31:36 -0400

    BORDURES SONT NICE!
    bordures sont regles!

---
+++ Assets/Scripts/GestionLimiteTerrain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GestionLimiteTerrain : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > 42|| transform.position.x <-42 ||
+           transform.position.z > 20 || transform.position.z < -20)
+        {
+            //transform.position = new Vector3(0, -1, 0);
+        }
+    }
+}
