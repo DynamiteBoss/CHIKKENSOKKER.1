7f0533f0e16697be30aa451cb653a4f210daded6
Sterling-qc <yologuy974@gmail.com>
2019-05-01 00:04:00 -0400

    items

--- Assets/Scripts/ScriptOeufMystère.cs
+++ Assets/Scripts/ScriptOeufMystère.cs
@@ -17,6 +17,8 @@ public class ScriptOeufMystère : NetworkBehaviour
 
     public const int IndiceMax = 8;
 
+    public string équipeLive;
+
     void Start()
     {    
     }
@@ -28,12 +30,11 @@ public class ScriptOeufMystère : NetworkBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        
         if (other.name == "Tête" && other.transform.parent.parent.tag == "Player")
         {
+
             Destroy(this.transform.gameObject);
             GameObject.Find("Main Camera").GetComponent<ScriptMécaniqueMatch>().nbOeufs -= 1;
-            //GetComponent<NetworkIdentity>().AssignClientAuthority(this.GetComponent<NetworkIdentity>().connectionToClient);
             //int random = UnityEngine.Random.Range(0, IndiceMax);
             indice = GameObject.FindGameObjectWithTag("Balle").GetComponent<ScriptBut>().random;
             //indice = UnityEngine.Random.Range(0, IndiceMax);
@@ -43,13 +44,11 @@ public class ScriptOeufMystère : NetworkBehaviour
         {
             Destroy(this.transform.gameObject);
             GameObject.Find("Main Camera").GetComponent<ScriptMécaniqueMatch>().nbOeufs -= 1;
-            //GetComponent<NetworkIdentity>().AssignClientAuthority(this.GetComponent<NetworkIdentity>().connectionToClient);
             //int random = UnityEngine.Random.Range(0, IndiceMax);
             indice = GameObject.FindGameObjectWithTag("Balle").GetComponent<ScriptBut>().random;
             //ndice = UnityEngine.Random.Range(0, IndiceMax);
             CmdAttribuerObjetJoueur(other.transform.parent.gameObject, indice);
         }
-        //GetComponent<NetworkIdentity>().RemoveClientAuthority(this.GetComponent<NetworkIdentity>().connectionToClient);
     }
     [Command]
     private void CmdAttribuerObjetJoueur(GameObject joueur, int indice)
@@ -57,6 +56,7 @@ public class ScriptOeufMystère : NetworkBehaviour
         Debug.Log(indice);
         if (joueur.GetComponent<TypeÉquipe>().estÉquipeA)
         {
+            équipeLive = "A";
             if (Inventaire.itemA2 == Inventaire.ITEMNUL && Inventaire.itemA1 != Inventaire.ITEMNUL)
             {
                 Inventaire.itemA2 = indice;
@@ -77,6 +77,7 @@ public class ScriptOeufMystère : NetworkBehaviour
         }
         else
         {
+            équipeLive = "B";
             if (Inventaire.itemB2 == Inventaire.ITEMNUL && Inventaire.itemB1 != Inventaire.ITEMNUL)
             {
                 Inventaire.itemB2 = indice;
@@ -103,13 +104,13 @@ public class ScriptOeufMystère : NetworkBehaviour
             if (position == 1)
             {
                 valeurItemTemp = Inventaire.itemA1;
-                positionSprite = new Vector3(-134, -96, -244);
+                positionSprite = new Vector3(-130, -104, -244);
                 Debug.Log("la position est " + positionSprite.ToString());
             }
             else
             {
                 valeurItemTemp = Inventaire.itemA2;
-                positionSprite = new Vector3(-218, -96, -244);
+                positionSprite = new Vector3(-205, -104, -244);
             }
         }
         else if (équipe == 'B')
@@ -117,137 +118,198 @@ public class ScriptOeufMystère : NetworkBehaviour
             if (position == 1)
             {
                 valeurItemTemp = Inventaire.itemB1;
-                positionSprite = new Vector3(132, -96, -244);
+                positionSprite = new Vector3(134, -104, -244);
             }
             else
             {
                 valeurItemTemp = Inventaire.itemB2;
-                positionSprite = new Vector3(217, -96, -244);
+                positionSprite = new Vector3(210, -104, -244);
             }
         }
 
         switch (valeurItemTemp)
         {
             case 0:
-                //GameObject Crotte3Sprite = (GameObject)Instantiate(Item.RetournerItemListe(8).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0));
                 GameObject Crotte3Sprite = (GameObject)Instantiate(Item.RetournerItemListe(8).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0), GameObject.Find("PnlPrincipal").transform);
                 Debug.Log("la position est " + positionSprite.ToString());
+
                 NetworkServer.Spawn(Crotte3Sprite);
                 Debug.Log("la position est " + positionSprite.ToString());
-                //CmdAfficherSprite(position, équipe, "crotte3");
+
+                if (équipeLive == "A")
+                {
+                    RpcSynchroParent(Crotte3Sprite);
+                }
+                else if (équipeLive == "B")
+                {
+                    CmdSynchroParent(Crotte3Sprite);
+                }
+                SynchroSprite(Crotte3Sprite);
+
                 return;
             case 1:
-                //GameObject Crotte5Sprite = (GameObject)Instantiate(Item.RetournerItemListe(9).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0));
                 GameObject Crotte5Sprite = (GameObject)Instantiate(Item.RetournerItemListe(9).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0), GameObject.Find("PnlPrincipal").transform);
                 Debug.Log("la position est " + positionSprite.ToString());
 
                 NetworkServer.Spawn(Crotte5Sprite);
                 Debug.Log("la position est " + positionSprite.ToString());
 
-                //CmdAfficherSprite(position, équipe, "crotte5");
+                if (équipeLive == "A")
+                {
+                    RpcSynchroParent(Crotte5Sprite);
+                }
+                else if (équipeLive == "B")
+                {
+                    CmdSynchroParent(Crotte5Sprite);
+                }
+                SynchroSprite(Crotte5Sprite);
+
                 return;
             case 2:
-                //GameObject OeufBlancXLSprite = (GameObject)Instantiate(Item.RetournerItemListe(11).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0));
                 GameObject OeufBlancXLSprite = (GameObject)Instantiate(Item.RetournerItemListe(11).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0), GameObject.Find("PnlPrincipal").transform);
                 Debug.Log("la position est " + positionSprite.ToString());
 
                 NetworkServer.Spawn(OeufBlancXLSprite);
                 Debug.Log("la position est " + positionSprite.ToString());
 
-                //CmdAfficherSprite(position, équipe, "oeufXL");
+                if (équipeLive == "A")
+                {
+                    RpcSynchroParent(OeufBlancXLSprite);
+                }
+                else if (équipeLive == "B")
+                {
+                    CmdSynchroParent(OeufBlancXLSprite);
+                }
+                SynchroSprite(OeufBlancXLSprite);
+
                 return;
             case 3:
-                //GameObject OeufBlanc3Sprite = (GameObject)Instantiate(Item.RetournerItemListe(10).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0));
                 GameObject OeufBlanc3Sprite = (GameObject)Instantiate(Item.RetournerItemListe(10).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0), GameObject.Find("PnlPrincipal").transform);
                 Debug.Log("la position est " + positionSprite.ToString());
+
 
                 NetworkServer.Spawn(OeufBlanc3Sprite);
                 Debug.Log("la position est " + positionSprite.ToString());
 
-                //CmdAfficherSprite(position, équipe, "oeuf3");
+                if (équipeLive == "A")
+                {
+                    RpcSynchroParent(OeufBlanc3Sprite);
+                }
+                else if (équipeLive == "B")
+                {
+                    CmdSynchroParent(OeufBlanc3Sprite);
+                }
+                SynchroSprite(OeufBlanc3Sprite);
+
                 return;
             case 4:
-                //GameObject OeufBrunSprite = (GameObject)Instantiate(Item.RetournerItemListe(12).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0));
                 GameObject OeufBrunSprite = (GameObject)Instantiate(Item.RetournerItemListe(12).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0), GameObject.Find("PnlPrincipal").transform);
                 Debug.Log("la position est " + positionSprite.ToString());
 
                 NetworkServer.Spawn(OeufBrunSprite);
                 Debug.Log("la position est " + positionSprite.ToString());
 
-                //CmdAfficherSprite(position, équipe, "torpille");
+                if (équipeLive == "A")
+                {
+                    RpcSynchroParent(OeufBrunSprite);
+                }
+                else if (équipeLive == "B")
+                {
+                    CmdSynchroParent(OeufBrunSprite);
+                }
+                SynchroSprite(OeufBrunSprite);
+
                 return;
             case 5:
-                //GameObject OeufBombeSprite = (GameObject)Instantiate(Item.RetournerItemListe(13).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0));
                 GameObject OeufBombeSprite = (GameObject)Instantiate(Item.RetournerItemListe(13).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0), GameObject.Find("PnlPrincipal").transform);
                 Debug.Log("la position est " + positionSprite.ToString());
 
                 NetworkServer.Spawn(OeufBombeSprite);
                 Debug.Log("la position est " + positionSprite.ToString());
 
-                //CmdAfficherSprite(position, équipe, "bombe");
+                if (équipeLive == "A")
+                {
+                    RpcSynchroParent(OeufBombeSprite);
+                }
+                else if (équipeLive == "B")
+                {
+                    CmdSynchroParent(OeufBombeSprite);
+                }
+                SynchroSprite(OeufBombeSprite);
+
                 return;
             case 6:
-                //GameObject OeufBrouilléSprite = (GameObject)Instantiate(Item.RetournerItemListe(14).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0));
                 GameObject OeufBrouilléSprite = (GameObject)Instantiate(Item.RetournerItemListe(14).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0), GameObject.Find("PnlPrincipal").transform);
                 Debug.Log("la position est " + positionSprite.ToString());
 
                 NetworkServer.Spawn(OeufBrouilléSprite);
                 Debug.Log("la position est " + positionSprite.ToString());
 
-                //CmdAfficherSprite(position, équipe, "brouillé");
+                if (équipeLive == "A")
+                {
+                    RpcSynchroParent(OeufBrouilléSprite);
+                }
+                else if (équipeLive == "B")
+                {
+                    CmdSynchroParent(OeufBrouilléSprite);
+                }
+                SynchroSprite(OeufBrouilléSprite);
+
                 return;
             case 7:
-                //GameObject VersDeTerreSprite = (GameObject)Instantiate(Item.RetournerItemListe(15).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0));
                 GameObject VersDeTerreSprite = (GameObject)Instantiate(Item.RetournerItemListe(15).ItemPhysique, positionSprite, Quaternion.Euler(41, 0, 0), GameObject.Find("PnlPrincipal").transform);
                 Debug.Log("la position est " + positionSprite.ToString());
 
                 NetworkServer.Spawn(VersDeTerreSprite);
                 Debug.Log("la position est " + positionSprite.ToString());
 
-                //CmdAfficherSprite(position, équipe, "versdeterre");
+                if (équipeLive == "A")
+                {
+                    RpcSynchroParent(VersDeTerreSprite);
+                }
+                else if (équipeLive == "B")
+                {
+                    CmdSynchroParent(VersDeTerreSprite);
+                }
+                SynchroSprite(VersDeTerreSprite);
+
                 return;
             default:
                 return;
         }
     }
+    [Command]
+    void CmdSynchroParent(GameObject item)
+    {
+        item.transform.SetParent(GameObject.Find("PnlPrincipal").transform);
+        RpcSynchroParent(item);
+    }
+    [ClientRpc]
+    void RpcSynchroParent(GameObject item)
+    {
+        item.transform.SetParent(GameObject.Find("PnlPrincipal").transform);  //CA TROUVE PAS LE PANEL
+    }
 
-    //[Command]
-    //private void CmdAfficherSprite(int position, char équipe, string nomSprite)
-    //{
-    //    if (équipe == 'A')
-    //    {
-    //        if (position == 1)
-    //        {
-    //            Inventaire.objet1A = Resources.Load<Sprite>("Image/" + nomSprite);
-    //            GameObject.Find("Objet1A").GetComponent<SpriteRenderer>().sprite = Inventaire.objet1A;
-    //            Debug.Log("Le Sprite" + nomSprite + "a été affiché en 1A");
-    //        }
-    //        else
-    //        {
-    //            Inventaire.objet2A = Resources.Load<Sprite>("Image/" + nomSprite);
-    //            GameObject.Find("Objet2A").GetComponent<SpriteRenderer>().sprite = Inventaire.objet2A;
-    //            Debug.Log("Le Sprite" + nomSprite + "a été affiché en 2A");
-    //        }
-    //    }
-    //    else if (équipe == 'B')
-    //    {
-    //        if (position == 1)
-    //        {
-    //            Inventaire.objet1B = Resources.Load<Sprite>("Image/" + nomSprite);
-    //            GameObject.Find("Objet1B").GetComponent<SpriteRenderer>().sprite = Inventaire.objet1B;
-    //            Debug.Log("Le Sprite" + nomSprite + "a été affiché en 1B");
-    //        }
-    //        else
-    //        {
-    //            Inventaire.objet2B = Resources.Load<Sprite>("Image/" + nomSprite);
-    //            GameObject.Find("Objet2B").GetComponent<SpriteRenderer>().sprite = Inventaire.objet2B;
-    //            Debug.Log("Le Sprite" + nomSprite + "a été affiché en 2B");
-
-    //        }
-    //    }
-    //    
-    //    //GameObject.Find("Objet" + position + équipe).GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Image/" + nomSprite);    }    
-    //}
+    void SynchroSprite(GameObject item)
+    {
+        CmdSynchroSprite(item);
+    }
+    [Command]
+    void CmdSynchroSprite(GameObject item)
+    {
+        Debug.Log(positionSprite.ToString() + "INITIAL");
+        item.transform.localPosition = positionSprite;
+        item.transform.localScale = new Vector3(8, 8, 8);
+        RpcSynchroSprite(item, positionSprite);
+        Debug.Log(positionSprite.ToString() + "FINAL");  
+    }
+    [ClientRpc]
+    void RpcSynchroSprite(GameObject item, Vector3 positionVrai)
+    {
+        Debug.Log(positionVrai.ToString() + "DANS LE RPC");  //CA MARQUE 0,0,0
+        item.transform.localPosition = positionVrai; //  NE MARCHE PAS
+        item.transform.localScale = new Vector3(8,8,8);  //  MARCHE PI LA ROTATION EST OK AUSSI
+    }
 
     void OnIndiceChange(int changement)
     {
